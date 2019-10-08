d927877c5823fb9da3dfda49881d997404be12df
ashesgeiv <2415277546@qq.com>
2019-10-08 18:51:25 +0800

    关于运算符的使用

--- Javascript/day01/script.js
+++ Javascript/day01/script.js
@@ -1,8 +1,8 @@
 // var创建变量
-var a = 070;
-var b = 'adadad1111pxdasdasdsa';
-var c = null;
-
-a= a.toString();
-document.write(typeof a);
-document.write(a);
+var a = 1;
+var b = '111';
+var c ;
+var d = null;
+c=b-1;
+document.write(typeof c);
+document.write(c);
