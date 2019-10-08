b92694a884497fe1b5fe584a3540a04cb45a8916
ashesgeiv <2415277546@qq.com>
2019-10-08 13:51:47 +0800

    关于Number，Boolean的强制类型转换 补充Number的各种进制的写法

--- Javascript/day01/script.js
+++ Javascript/day01/script.js
@@ -1,7 +1,8 @@
 // var创建变量
-var a = 111;
-var b = 'helloworld"dddd"';
-var c =null;
-a=String(a);
+var a = 070;
+var b = 'adadad1111pxdasdasdsa';
+var c = null;
+
+a= a.toString();
 document.write(typeof a);
 document.write(a);
