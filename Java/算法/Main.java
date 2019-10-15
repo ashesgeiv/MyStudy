a17a550c5b819a582685a02de3e12ed4d0904b44
ashesgeiv <43413310+ashesgeiv@users.noreply.github.com>
2019-10-15 16:21:20 +0800

    t't't

--- Java/算法/Main.java
+++ Java/算法/Main.java
@@ -1,21 +1,93 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+/**
+ * Main
+ */
+
+class Element {
+  static char data;
+  static Element pointer=null;
+}
+
+class LinkList1 {
+
+  Element head;
+  Element pElement;
+
+ 
+  LinkList1() {
+    
+    
+  }
+
+  
+  Element makeNewElement() {
+    return new Element();
+  }
+
+ 
+
+  void put(char data) {
+    Element p = makeNewElement();
+    p.data = data;
+    pElement.pointer = p;
+    pElement = p;
+     System.out.println("-1");
+  }
+
+ 
+  void put(char data, int num) {
+   
+    Element p = chaXun(num);
+    Element pNew = makeNewElement();
+    pNew.data = data;
+    pNew.pointer = p.pointer;
+    p.pointer = pNew;
+  }
+
+  
+  void delete(int num) {
+    Element p = chaXun(num - 1);
+    Element pNew = p.pointer;
+    p.pointer = pNew.pointer;
+  }
+
+ 
+  Element chaXun(int num) {
+
+    Element p = head.pointer;
+    for (int i = 0; i != num || p.pointer != null; i++) {
+      p = p.pointer;
+      System.out.println(i);
+    }
+    return p;
+  }
+
+ 
+  void tongJi() {
+    int i = 0;
+    Element p = head.pointer;
+    if (p== null) {
+      System.out.println("-1");
+    }
+    
+    for (; p.pointer != null; i++) {
+      p = p.pointer;
+    }
+    System.out.println(i);
+  }
+  void show(){
+    
+    Element p = head.pointer;
+    while (p.pointer!=null) {
+      System.out.println(p.data);
+      p=p.pointer;
+    }
+  }
+}
 
 public class Main {
   public static void main(String[] args) {
-    List<>
-    Map<String, String> map = new HashMap<>();
-    map.put("you", "aaaa","mark up");
-    map.put("HTML", "mark up");
-    map.put("Oracle", "database");
-    map.put("XML", "data");
-
-    // Get  the   set of  keys
-    Set<String> keys  = map.keySet();
-
-    // Print all keys  using the   forEach()  method.
-    keys.forEach(System.out::println);
+    LinkList1 list1 =new LinkList1();
+    list1.put('x');
+    list1.show();
   }
 }
